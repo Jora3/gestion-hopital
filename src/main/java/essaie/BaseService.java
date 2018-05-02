8bb589275016d12a065ec4d0503ae09b74b4a03c
Olivier <olivier.aps.p2@gmail.com>
2018-05-02 11:34:02 +0300

    utilisation de la Classe BaseService

--- src/main/java/essaie/BaseService.java
+++ src/main/java/essaie/BaseService.java
@@ -1,35 +1,12 @@
 package essaie;
 
 import dao.GenericDAO;
-import modele.BaseModele;
 import modele.Mouvement;
-
-import java.util.List;
 
 public class BaseService {
     private final GenericDAO genericDAO = new GenericDAO();
 
-    public void save(BaseModele modele){
-        try {
-            genericDAO.save(modele);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public List<BaseModele> findAll(BaseModele modele) throws Exception {
-        return genericDAO.findAll(modele);
-    }
-
-    public BaseModele findById(BaseModele modele) throws Exception {
-        return null;//genericDAO.findById(modele);
-    }
-
-    public void saveMouvement(Mouvement mouvement){
-        try {
-            genericDAO.save(mouvement);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void save(Mouvement mouvement) throws Exception {
+        genericDAO.save(mouvement);
     }
 }
